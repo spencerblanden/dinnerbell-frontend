edec4ceda70d86d1460e1c95e80726ceaf617e95
Spencer Blanden <89828067+spencerblanden@users.noreply.github.com>
2021-10-28 20:45:52 -0500

    Add logo and started to work on nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,23 +2,39 @@ import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Divider from '@material-ui/core/Divider';
 import { makeStyles } from '@material-ui/core/styles';
+
 import { Link } from 'react-router-dom';
 // import { ColumnToRow, Row, Item } from '@mui-treasury/components/flex';
 // import { NavMenu, NavItem } from '@mui-treasury/components/menu/navigation';
 
+const navStyle = makeStyles(() => ({
+    outerBox: {
 
+    }
+}))
 
 
 const Nav = (props) => {
+    const styles = navStyle();
     return (
-        <header className="nav">
+        <navStyle>
+           <Box className={styles.outerBox}>
+           <Container>
+            <Box>
+                <img src='../DinnerBellLogo.png' alt=''/>
+            </Box> 
+            <Divider />
+            <Box>
             <Link to="/">
                 <h1>Home</h1>
             </Link>
             <Link to="/menu">
                 <h1>Menu</h1>
             </Link>
-        </header>
+            </Box>
+           </Container>
+           </Box>
+        </navStyle>
     );
 }
 
