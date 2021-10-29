5aabb4867d2628231bf346a695f2d7176ddbdb51
Spencer Blanden <89828067+spencerblanden@users.noreply.github.com>
2021-10-28 22:39:50 -0500

    updated the nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,30 +1,55 @@
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Divider from '@material-ui/core/Divider';
-import { makeStyles } from '@material-ui/core/styles';
+import styled from 'styled-components';
+import { RowingTwoTone } from '@material-ui/icons';
+import { findByLabelText } from '@testing-library/react';
 
 import { Link } from 'react-router-dom';
 // import { ColumnToRow, Row, Item } from '@mui-treasury/components/flex';
 // import { NavMenu, NavItem } from '@mui-treasury/components/menu/navigation';
 
-const navStyle = makeStyles(() => ({
-    outerBox: {
-
-    }
-}))
+const NavStyle = styled.header`
+       .outerBox {
+            display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
+        background-color: whitesmoke;
+        height: 7rem;
+        width: 100%;
+        .links{
+           display: flex;
+           justify-content: flex-end;
+           align-items: flex-end;
+           
+                font-size: 1rem;
+           a{ text-decoration: none;
+            color: inherit;
+            margin-right: 16px;
+        }
+       }
+       }
+       img{
+         height: 100px;
+            width: 100px;
+            position: absolute;
+       }
+       
+`;
 
 
 const Nav = (props) => {
-    const styles = navStyle();
+
     return (
-        <navStyle>
-           <Box className={styles.outerBox}>
+        <NavStyle>
+           <Box className='outerBox'>
            <Container>
-            <Box>
+            <Box className='logo'>
                 <img src='../DinnerBellLogo.png' alt=''/>
             </Box> 
             <Divider />
-            <Box>
+            <Box className='links'>
             <Link to="/">
                 <h1>Home</h1>
             </Link>
@@ -34,7 +59,7 @@ const Nav = (props) => {
             </Box>
            </Container>
            </Box>
-        </navStyle>
+        </NavStyle>
     );
 }
 
